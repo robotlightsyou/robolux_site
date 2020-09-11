9bfe5d5729f9a72c6b42ed3212b9828029e24b29
Adam Earle <robotlightsyou@gmail.com>
2020-09-11 13:03:03 -0700

    reclassed views landing/flashcards

--- robosite/flashcards/models.py
+++ robosite/flashcards/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Term(models.Model):
+    name = models.CharField(max_length = 100)
+    defi = models.TextField()
+    deck = models.CharField(max_length = 100, default='General')
+
+    def __str__(self):
+        return self.name
+
+
